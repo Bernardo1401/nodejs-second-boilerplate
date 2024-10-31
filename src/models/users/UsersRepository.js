d10c04b6736260298548cd5a4d1b05eafbd8d2eb
Bernardo Marques <sardinhaber@gmail.com>
2024-10-31 15:23:50 -0300

    feat: create get all users method

--- src/models/users/UsersRepository.js
+++ src/models/users/UsersRepository.js
@@ -5,4 +5,21 @@ class UsersRepository {
     getAllUsers() {
         return this.users;
     }
+    getById(id) {
+        return this.users.find(user => user.id === id);
+    }
+    add(user) {
+        this.users.push(user);
+    }
+    update(user) {
+        const index = this.users.findIndex(u => u.id === user.id);
+        this.users[index] = user;
+    }
+    delete(id) {
+        const index = this.users.findIndex(u => u.id === id);
+        this.users.splice(index, 1); 
+        return true;
+    }    
 }
+
+export default UsersRepository;
